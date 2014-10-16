50d26a4359b23ffd7dacc2d5c27a327599ed252b
Dimitar Dimitrov <me@ddimitrov.name>
2014-10-17 00:48:30 +0300

    Add the solution and full specs for task 01

--- tasks/01/spec.rb
+++ tasks/01/spec.rb
@@ -1,19 +1,27 @@
-describe 'series' do
-  it 'handles fibonacci expect(series' do
-    expect(series('fibonacci', 1)).to eq 1
-    expect(series('fibonacci', 20)).to eq 6765
+require_relative 'solution'
+
+describe "series" do
+  it "handles fibonacci series" do
+    series('fibonacci', 1).should eq 1
+    series('fibonacci', 5).should eq 5
+    series('fibonacci', 10).should eq 55
+    series('fibonacci', 15).should eq 610
+    series('fibonacci', 20).should eq 6765
   end
 
-  it 'handles lucas expect(series' do
-    expect(series('lucas', 1)).to eq 2
-    expect(series('lucas', 2)).to eq 1 
-    expect(series('lucas', 3)).to eq 3
-    expect(series('lucas', 4)).to eq 4
-    expect(series('lucas', 15)).to eq 843 
+  it "handles lucas series" do
+    series('lucas', 1).should eq 2
+    series('lucas', 5).should eq 7
+    series('lucas', 10).should eq 76
+    series('lucas', 15).should eq 843
+    series('lucas', 20).should eq 9349
   end
 
-  it 'handles summed expect(series' do
-    expect(series('summed', 1)).to eq 3
-    expect(series('summed', 15)).to eq 1453 
+  it "handles summed series" do
+    series('summed', 1).should eq 3
+    series('summed', 5).should eq 12
+    series('summed', 10).should eq 131
+    series('summed', 15).should eq 1453
+    series('summed', 20).should eq 16114
   end
 end
