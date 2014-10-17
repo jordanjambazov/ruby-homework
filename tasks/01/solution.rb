9af546c2b8df0a136174c0b5453e047dd95b9288
Jordan Jambazov <jordan.jambazov@strypes.eu>
2014-10-17 17:14:04 +0300

    Add my version of the homework

--- tasks/01/solution.rb
+++ tasks/01/solution.rb
@@ -1,15 +1,17 @@
-def generic_series(n, first_value, second_value)
-  return first_value if n == 1
-  return second_value if n == 2
-
-  generic_series(n - 1, first_value, second_value) +
-  generic_series(n - 2, first_value, second_value)
+def fibonacci(number)
+  return number if number <= 1
+  fibonacci(number - 1) + fibonacci(number - 2)
 end
 
-def series(type, n)
-  case type
-  when 'fibonacci' then generic_series(n, 1, 1)
-  when 'lucas'     then generic_series(n, 2, 1)
-  when 'summed'    then series('fibonacci', n) + series('lucas', n)
-  end
+def lucas(number)
+  return 2 if number == 1
+  return 1 if number == 2
+  lucas(number - 1) + lucas(number - 2)
+end
+
+def series(type, number)
+  result = 0
+  result += fibonacci(number) if type == 'fibonacci' or type == 'summed'
+  result += lucas(number) if type == 'lucas' or type == 'summed'
+  result
 end
